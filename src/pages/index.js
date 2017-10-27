7bcf7fa5a6537c2177178bc21170ebcce6bb1858
evert.saar <evertisland@hotmail.com>
2017-10-27 23:20:51 +0300

    Remove failing files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import styled from 'styled-components'
-import ui from '../layouts/theme'
-import backgroundImage from '../../static/images/dogs.jpg'
 
-const Title = styled.h1`
-  color: ${ui.color.background};
-`
-
-const IndexPage = ({ className }) => (
-  <div className={className}>
-    <Title>Homes</Title>
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
 
-export default styled(IndexPage)`
-  height: 100%;
-  width: 100%;
-  flex: 1;
-  display: flex;
-  background-image: url(${backgroundImage});
-  background-size: cover;
-  background-position: center;
-  padding: ${ui.size.s};
-`
+export default IndexPage
