a75a23661478bf83f0cea95915881b62ead5815a
evert.saar <evertisland@hotmail.com>
2017-10-27 23:47:50 +0300

    Change styled-components def style

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,26 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 import ui from '../layouts/theme'
-//import backgroundImage from '../../static/images/dogs.jpg'
+import backgroundImage from '../../static/images/dogs.jpg'
 
 const Title = styled.h1`
   color: ${ui.color.background};
 `
-
-const IndexPage = ({ className }) => (
-  <div className={className}>
-    <Title>Homes</Title>
-  </div>
-)
-
-export default styled(IndexPage)`
+const Container = styled.div`
   height: 100%;
   width: 100%;
   flex: 1;
   display: flex;
-  //background-image: url(${backgroundImage});
+  background-image: url(${backgroundImage});
   background-size: cover;
   background-position: center;
   padding: ${ui.size.s};
 `
+
+const IndexPage = () => (
+  <Container>
+    <Title>Homes</Title>
+  </Container>
+)
+
+export default IndexPage
