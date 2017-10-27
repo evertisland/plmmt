1f4d7e11168aecfd4f21c972f11b0625a10570fb
evert.saar <evertisland@hotmail.com>
2017-10-27 22:56:32 +0300

    Update configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-  },
-  plugins: [`gatsby-plugin-react-helmet`],
+	siteMetadata: {
+		title: `Paul Emmet`,
+		author: `Paul Emmet`
+	},
+	plugins: [
+		`gatsby-plugin-styled-components`,
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/src/pages/posts`,
+				name: 'posts',
+			},
+		},
+		{
+			resolve: 'gatsby-transformer-remark',
+			options: {
+				plugins: []
+			}
+		},
+	],
 }
