6f74b81a0da2658f85ce81c117f8839875ad5298
evert.saar <evertisland@hotmail.com>
2017-10-28 00:07:39 +0300

    Rearrange

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+import ui from '../layouts/theme'
+import backgroundImage from '../../static/images/dogs.jpg'
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  flex: 1;
+  display: flex;
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  background-position: center;
+  padding: ${ui.size.s};
+  h1 {
+    color: ${ui.color.background};
+  }
+`
 
 const IndexPage = () => (
-  <div>
+  <Container>
     <h1>Homes</h1>
-  </div>
+  </Container>
 )
 export default IndexPage
+
+// import React from 'react'
+//
+// const IndexPage = () => (
+//   <div>
+//     <h1>Homes</h1>
+//   </div>
+// )
+//
+// export default IndexPage
