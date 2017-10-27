9278b8cc245bbc08b97ce29147b10448359bce71
evert.saar <evertisland@hotmail.com>
2017-10-27 23:59:25 +0300

    Test tiny index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,32 @@
 import React from 'react'
-import styled from 'styled-components'
-//import ui from '../layouts/theme'
-//import backgroundImage from '../../static/images/dogs.jpg'
+// import styled from 'styled-components'
+// import ui from '../layouts/theme'
+// import backgroundImage from '../../static/images/dogs.jpg'
+//
+// const Title = styled.h1`
+//   color: ${ui.color.background};
+// `
+//
+// const IndexPage = ({ className }) => (
+//   <div className={className}>
+//     <Title>Homes</Title>
+//   </div>
+// )
+// export default IndexPage
+// export default styled(IndexPage)`
+//   height: 100%;
+//   width: 100%;
+//   flex: 1;
+//   display: flex;
+//   background-image: url(${backgroundImage});
+//   background-size: cover;
+//   background-position: center;
+//   padding: ${ui.size.s};
+// `
 
 const IndexPage = ({ className }) => (
   <div className={className}>
-    <h1>Home page</h1>
+    <Title>Homes</Title>
   </div>
 )
-
-export default styled(IndexPage)`
-  height: 100%;
-  width: 100%;
-  flex: 1;
-  display: flex;
-  //background-image: url(${backgroundImage});
-  background-size: cover;
-  background-position: center;
-  //padding: ${ui.size.s};
-`
+export default IndexPage
